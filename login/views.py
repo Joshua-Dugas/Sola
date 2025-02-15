f5fdfacb390f6881e76319dd7c5ee33053f1b888
Papa Yesh <62230688+Joshua-Dugas@users.noreply.github.com>
2025-02-15 12:45:30 -0600

    Changed formatting to bootstrap | added login form | created users table

--- login/views.py
+++ login/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def login(request):
@@ -8,5 +10,15 @@ def login(request):
     return HttpResponse(template.render())
 
 def create_account(request):
-    template = loader.get_template('create_account.html')
-    return HttpResponse(template.render())
+    #If we get a post request we will accept the form data, else create blank form
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account created for {username}!")
+            return redirect('login')
+
+
+    else:
+        form = UserCreationForm()
+    return render(request, 'create_account.html', {'form': form})
